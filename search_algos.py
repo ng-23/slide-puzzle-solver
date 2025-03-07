82fd9da0e67c7d450c4812df8284fa4266bb856b
ng-23 <23grattan@gmail.com>
2025-03-07 16:16:15 -0500

    Created module to contain search algos

---
+++ search_algos.py
@@ -0,0 +1,41 @@
+'''
+Search algorithms
+'''
+
+def precomputed_bfs(graph:dict[tuple,dict[tuple,tuple[int,int]]], goal_state:tuple[tuple]):
+    '''
+    Perform a breadth-first search on a precomputed graph search space
+
+    `graph` is expected to (generally) look like so:
+
+    {
+        state0: {state1: (i,j), state2: (i,j)},
+        ...
+        stateN: {stateN+1: (i,j), stateN+2: (i,j)}
+    }
+    '''
+
+    # see https://stackoverflow.com/questions/30362391/how-to-find-the-first-key-in-a-dictionary-python
+    queue = [(next(iter(graph)), [])] # tuple of current state, moves made to reach current state
+
+    seen_states = set([next(iter(graph))])
+
+    while queue:
+        curr_state, moves_made = queue.pop(0)
+
+        if curr_state == goal_state:
+            return moves_made, len(moves_made)
+        
+        unseen_states = set(graph[curr_state].keys()) - seen_states
+        for unseen_state in unseen_states:
+            move_to = graph[curr_state][unseen_state]
+
+            seen_states.add(unseen_state)
+            queue.append(
+                (
+                    unseen_state,
+                    moves_made + [move_to]
+                ),
+            )
+
+    return None, len(seen_states)
