456fa44a3eae291f080bad42efab4ccc306f7597
ng-23 <23grattan@gmail.com>
2025-03-09 20:32:24 -0400

    New function stub
    Added function stub for greedy breadth-first search (aka best-first search)

--- search_algos.py
+++ search_algos.py
@@ -4,7 +4,7 @@ Search algorithms
 
 def precomputed_bfs(graph:dict[tuple,dict[tuple,tuple[int,int]]], goal_state:tuple[tuple]):
     '''
-    Perform a breadth-first search on a precomputed graph search space
+    Perform a breadth-first search on a precomputed search space graph
 
     `graph` is expected to (generally) look like so:
 
@@ -42,7 +42,7 @@ def precomputed_bfs(graph:dict[tuple,dict[tuple,tuple[int,int]]], goal_state:tup
 
 def precomputed_dfs(graph:dict[tuple,dict[tuple,tuple[int,int]]], goal_state:tuple[tuple]):
     '''
-    Perform a depth-first search on a precomputed graph search space
+    Perform a depth-first search on a precomputed search space graph
 
     `graph` is expected to (generally) look like so:
 
@@ -76,3 +76,24 @@ def precomputed_dfs(graph:dict[tuple,dict[tuple,tuple[int,int]]], goal_state:tup
             )
 
     return None, len(seen_states)
+
+def precomputed_gbfs(graph:dict[tuple,dict[tuple,tuple[int,int]]], goal_state:tuple[tuple]):
+    '''
+    Perform a greedy breadth-first search on a precomputed search space graph (with edge costs)
+
+    `graph` is expected to (generally) look like so:
+
+    ```
+    {
+        state0: {state1: (i,j,cost1), state2: (i,j,cost2)},
+        ...
+        stateN: {stateN+1: (i,j,costN+1), stateN+2: (i,j,costN+2)}
+    }
+    ```
+    
+    General cost function: f(n) = h(n)
+    - n is a node, which in this case is a state
+    - f(n) is the cost function
+    - h(n) is the heuristic function, which generally estimates the "cost" of going from the current state to the goal state
+    '''
+    pass
